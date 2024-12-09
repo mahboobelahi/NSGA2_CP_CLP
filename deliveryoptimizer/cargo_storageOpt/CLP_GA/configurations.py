880041c7d03057571d4cffe4f0f7af5228009d04
Mahboob Elahi (Personal) <mahboobelahi93@gmail.com>
2024-12-09 16:32:43 +0200

    GA and other configurations

---
+++ deliveryoptimizer/cargo_storageOpt/CLP_GA/configurations.py
@@ -0,0 +1,12 @@
+config_dict = {
+    "NUM_OF_INDIVIDUALS": 50,
+    "NUM_OF_GENERATIONS": 1,
+    "PC": int(0.8 * 50),  #0.8
+    "PM1": 0.6,
+    "PM2": 0.3,
+    "K": 4,
+    "ROTATIONS": {
+        0: 2,
+        1: 4,
+        2: 6
+    }}
