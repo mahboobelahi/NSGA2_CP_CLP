353e819bdace63231494084e67e2cb8d2c650892
Mahboob Elahi (Personal) <mahboobelahi93@gmail.com>
2024-12-11 18:55:27 +0200

    item rotations

---
+++ deliveryoptimizer/cargo_storageOpt/CLP_GA/rotation.py
@@ -0,0 +1,11 @@
+class RotationType:
+
+    RT_LWH = 0
+    RT_WLH = 1
+    RT_LHW = 2
+    RT_WHL = 4
+    RT_HLW = 3 #vertical
+    RT_HWL = 5 #vertical
+    
+
+    ALL = [RT_LWH, RT_WLH, RT_LHW, RT_HLW, RT_WHL, RT_HWL]
